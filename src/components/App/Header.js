aae434edb898c03a12cc36f263e3b7b266a6fa92
Jared DiScipio <jared.michael.discipio@gmail.com>
2021-01-14 13:03:25 -0500

    completed

---
+++ src/components/App/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header, Icon } from 'semantic-ui-react'
+const MyHeader = () => (
+  <Header as='h2'>
+      <Icon name="coffee" size="big" />
+      Coffee Ping
+  </Header>
+)
+
+export default MyHeader
+
